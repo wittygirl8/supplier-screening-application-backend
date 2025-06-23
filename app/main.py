32d46fee4ef06751bdb2fb5677ab0385189c6fea
Riddhi K Singh <Riddhi.Singh@in.ey.com>
2025-06-23 09:27:46 +0530

    push

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 # Guards against HTTP Host Header attacks
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=get_settings().security.allowed_hosts,
+    allowed_hosts=["*"],
 )
 
 
